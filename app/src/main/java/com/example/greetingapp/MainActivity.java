68ee3b30470eb26f6e507eb6f4475727355b3151
itu-itis23-kan22 <kan22@itu.edu.tr>
2025-03-31 12:25:22 +0200

    Take a picture code is written

--- app/src/main/java/com/example/greetingapp/MainActivity.java
+++ app/src/main/java/com/example/greetingapp/MainActivity.java
@@ -14,6 +14,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import java.util.Random;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.widget.Toast;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +34,62 @@ public class MainActivity extends AppCompatActivity {
     private TextView greetingTextView;
     private androidx.constraintlayout.widget.ConstraintLayout mainLayout;
 
+    private Button takePictureButton;
+    private String currentPhotoPath;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            // Galeriye ekle
+            galleryAddPic();
+            Toast.makeText(this, "Fotoğraf çekildi ve galeriye kaydedildi", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                Toast.makeText(this, "Error creating file", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        getPackageName() + ".provider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +97,20 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        // View'leri bağla
+        takePictureButton = findViewById(R.id.button);
         nameEditText = findViewById(R.id.nameEditText);
         greetButton = findViewById(R.id.greetButton);
         greetingTextView = findViewById(R.id.greetingTextView);
-
-
         mainLayout = findViewById(R.id.main);
+
+        // Butonlara listener ekle
+        takePictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
 
         greetButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,7 +135,6 @@ public class MainActivity extends AppCompatActivity {
                 greetButton.setBackgroundColor(colorButton);
                 greetButton.setTextColor(colorText);
                 greetingTextView.setTextColor(colorGreetingText);
-
             }
         });
 
